e9c0e7d6c9511ece6963f07473d28a61173e5d22
Husnain Mehar <husnainmehar0@outlook.com>
2024-03-20 02:08:33 +0500

    Day 1 Lectures

---
+++ FirstClass.java
@@ -0,0 +1,10 @@
+//Program to print star pattern in Java without using loops
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,15 @@
+
+public class Variable {
+    public static void main(String[] args) {
+        // variables
+        int a = 20; // Use to store integer
+        String name = "Tony Stark"; // Use to store Text
+        Double pin = 20.00021234411111111; // Use to store Decimal with large size
+        byte bit = 120; //only store valuse less than or equal to q byte
+        short sh = 16000; //use to store short integers
+        //Float ge = 20.0002111; // Use to store Decimal upto 7 decimal points
+        long lg = 1374947597; //Use to store long interger valuse
+        char character = 'C'; //only stores single character
+        Boolean bool; //Give only two outputs 0,1 or true,false
+    }
+}
